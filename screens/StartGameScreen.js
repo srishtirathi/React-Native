f46dcf8ed27aa413c5b9ce674b490711a8a4b169
srishtirathi <srishti.cs17@bmsce.ac.in>
2022-02-04 15:10:17 +0530

    Working on the Layout

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const StartGameScreen = ( props ) =>
 {
     return (
         <View style={ styles.screen }>
-            <Text>dummy</Text>
+            <Text style={ styles.title }>Start A New Game!</Text>
+            <View style={ styles.inputContainer }>
+                <Text>Enter A Number</Text>
+                <TextInput />
+                <View style={ styles.buttonContainer }>
+                    <Button title="Rest" onPress={ () => { } } />
+                    <Button title="Confirm" onPress={ () => { } } />
+                </View>
+            </View>
         </View> )
 
 }
@@ -16,6 +24,22 @@ const styles = StyleSheet.create( {
         padding: 10,
         alignItems: 'center',
     },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+    },
+    title: {
+        fontSize: 24,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+
+    }
 } );
 
 export default StartGameScreen;
